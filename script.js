6448fa8e7d10486d88b046686583a2ae4a5b95e4
Impana-g <impana.948@gmail.com>
2026-01-15 22:08:10 +0530

    added script js

---
+++ script.js
@@ -0,0 +1,37 @@
+const questions = [
+  {
+      question:"Which is the largest animal in the world?",
+      answers:[
+          {text:"Shark", correct:false},
+          {text:"Blue Whale", correct:true},
+          {text:"Giraffe", correct:false},
+          {text:"Elephant", correct:false}
+      ]
+  },
+  {
+      question:"What is the capital of France?",
+      answers:[
+          {text:"Berlin", correct:false},   
+          {text:"Madrid", correct:false},
+          {text:"Paris", correct:true},
+          {text:"Rome", correct:false}
+      ]
+  },
+  {
+      question:"Which planet is known as the Red Planet?",
+      answers:[
+          {text:"Earth", correct:false},   
+          {text:"Mars", correct:true},
+          {text:"Jupiter", correct:false},
+          {text:"Venus", correct:false}
+      ]
+  },{
+      question:"Who wrote 'Romeo and Juliet'?",
+      answers:[
+          {text:"Charles Dickens", correct:false},  
+          {text:"William Shakespeare", correct:true},
+          {text:"Mark Twain", correct:false},
+          {text:"Jane Austen", correct:false}
+      ]
+  }
+];
